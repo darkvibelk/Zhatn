e4ebc78b0d6e6eb419a3b0f1f6462501fe9bc789
Mohamed Zuhail <mohamed.zuhail@maytech.au>
2025-12-17 20:30:37 +0530

    Fix: Converted tailwind.config.js to ESM syntax to resolve missing animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import tailwindcssAnimate from 'tailwindcss-animate';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -8,6 +10,6 @@ export default {
     extend: {},
   },
   plugins: [
-    require("tailwindcss-animate")
+    tailwindcssAnimate
   ],
 }
